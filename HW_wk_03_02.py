61f9e219719ee514346197185fab4549981936e4
davkim412 <71366799+davkim412@users.noreply.github.com>
2020-10-10 18:14:30 +0900

    Update HW_wk_03_02.py
    commit 확인을 위한 변경

--- HW_wk_03_02.py
+++ HW_wk_03_02.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-
+   
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
 # 아래 빈 칸('')을 채워보세요
